9fcdd1419cabb0638590c72f579cf2e2b844d021
zhenggang.qiu <changmuqiu@gmail.com>
2018-11-26 10:52:33 +0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,13 @@
 # set -x
 set -e # 有错误则退出
 
-repo_url='git@github.com:changmu/_qzg.git'
+# 配置远程仓库地址
+repo_url='https://github.com/changmu/_qzg.git'
+my_name=`whoami`
+if $my_name = "zhenggang.qiu" || $my_name = "user_00"; then
+    repo_url='https://git.yingzhongtong.com/zhenggang.qiu/_qzg.git'
+fi
+echo repo_url is [$repo_url]
 
 ### 下载配置
 if [ -d $HOME/.qzg ]; then
