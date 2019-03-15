89ca984d515c7752520dad2c7d88ee6628fff3f0
zhenggang.qiu <changmuqiu@gmail.com>
2019-03-15 09:57:18 +0800

    update

--- init.sh
+++ init.sh
@@ -11,19 +11,19 @@ export PATH=/bin:$PATH
 export GIT_TERMINAL_PROMPT=1
 # 添加go代理
 export GOPROXY=https://goproxy.io
+export ETCDCTL_API=3
 
 ##############################################################################
 # alias
 ##############################################################################
 alias s='ssh'
+alias vi='vim'
 alias g='git'
 alias tf='tail -f'
 alias ll='ls -lah'
 alias up='git cm -am "auto save and push"; g push'
 alias aup='git add .; up'
 alias gcm='git pull && cmake .. && make -j4'
-alias m="cd $BASE_MYAPI_PATH"
-alias p="cd $BASE_PROTOLIB_PATH"
 alias .q="cd $HOME/.qzg"
 
 ##############################################################################
