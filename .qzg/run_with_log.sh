83d61ae0caba0c66b61884e79f6d31f4fc577fb2
windqiu <windqiu@futunn.com>
2026-01-13 21:38:21 +0800

    feature: 添加crontab任务执行包装脚本
    添加run_with_log.sh用于crontab任务执行时记录时间戳日志，
    便于追踪定时任务的开始时间、结束时间和退出状态。

---
+++ .qzg/run_with_log.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 带时间戳的 crontab 任务执行包装脚本
+
+LOG_FILE="/home/windqiu/log/crontab.log"
+CMD_NAME="$1"
+shift
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') [START] $CMD_NAME" >> "$LOG_FILE"
+"$@" >> "$LOG_FILE" 2>&1
+EXIT_CODE=$?
+echo "$(date '+%Y-%m-%d %H:%M:%S') [END] $CMD_NAME (exit: $EXIT_CODE)" >> "$LOG_FILE"
+echo "----------------------------------------" >> "$LOG_FILE"
